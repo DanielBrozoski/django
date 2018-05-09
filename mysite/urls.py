3e72b036564adc9a865ef7717621079573fbea9e
João Paulo Zorek <joaopaulozorek@gmail.com>
2018-05-09 16:13:38 -0300

    Alterando o urls

--- mysite/urls.py
+++ mysite/urls.py
@@ -3,10 +3,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-<<<<<<< HEAD
-    url(r'', include('eventos.urls')),
     url(r'', include('enquete.urls')),
-=======
     url(r'^evento/', include('eventos.urls')),
->>>>>>> 2f2f61dff847480f167f9a4901b2143a76b1505c
 ]
